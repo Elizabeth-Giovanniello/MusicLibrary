dcec887dc27065ac007e9e7a2c50c3b421aa8a46
Lizzy <ea.giovanniello@gmail.com>
2022-02-14 17:27:43 -0500

    Implemented PageHeader component. Added dropdown button for user to select their search type.

--- src/Components/PageHeader/PageHeader.jsx
+++ src/Components/PageHeader/PageHeader.jsx
@@ -4,9 +4,35 @@ const PageHeader = (props) => {
 
     const [searchType, setSearchType] = useState('keyword');
 
-    return ( 
 
-     );
+    return ( 
+        <div>
+            <div className="dropdown">
+                <button>Search Category</button>
+                <ul className="dropdown-menu">
+                    <li className="dropdown-item" onClick={setSearchType('genre')}>Genre</li>
+                    <li className="dropdown-item" onClick={setSearchType('album')}>Album</li>
+                    <li className="dropdown-item" onClick={setSearchType('artist')}>Artist</li>
+                </ul>
+            </div>
+            {(() => {
+                switch(searchType) {
+                    case 'keyword':
+                        <SearchBar />
+                        break;
+                    case 'genre':
+                        <SearchBar />
+                        break;
+                    case 'album':
+                        <SearchBar />
+                        break;
+                    case 'artist':
+                        <SearchBar />
+                        break;
+                }
+            })()}
+        </div>
+    );
 }
  
 export default PageHeader;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Popper from '@popperjs/core';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 ReactDOM.render(
