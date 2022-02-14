d73894c81d6caf3f308bdf3a60f0f46180cb86cd
Lizzy <ea.giovanniello@gmail.com>
2022-02-14 16:59:22 -0500

    Created functions to search by each type of song property.

--- src/Components/DisplayMusic/DisplayMusic.jsx
+++ src/Components/DisplayMusic/DisplayMusic.jsx
@@ -10,15 +10,49 @@ const DisplayMusic = (props) => {
 
     function searchByKeyword(keyword){
         let displayedMusic = props.musicLibrary.filter(function(song){
-            return song.title.toLowerCase().includes(keyword) || song.artist.toLowerCase().includes(keyword) || song.album.toLowerCase().includes(keyword) || song.genre.toLowerCase().includes(keyword) || song.releaseDate.toLowerCase().includes(keyword);
+            return song.title.toLowerCase().includes(keyword.toLowerCase()) || song.artist.toLowerCase().includes(keyword.toLowerCase()) || song.album.toLowerCase().includes(keyword.toLowerCase()) || song.genre.toLowerCase().includes(keyword.toLowerCase()) || song.releaseDate.includes(keyword);
         })
         setMusicToDisplay(displayedMusic);
     }
 
+    function searchByGenre(genre){
+        let displayedMusic = props.musicLibrary.filter(function(song){
+            return song.genre.toLowerCase().includes(genre.toLowerCase());
+        })
+        setMusicToDisplay(displayedMusic);
+    }
+
+    function searchByArtist(artist){
+        let displayedMusic = props.musicLibrary.filter(function(song){
+            return song.artist.toLowerCase().includes(artist.toLowerCase());
+        })
+        setMusicToDisplay(displayedMusic);
+    }
+
+    function searchByAlbum(album){
+        let displayedMusic = props.musicLibrary.filter(function(song){
+            return song.album.toLowerCase().includes(album.toLowerCase());
+        })
+        setMusicToDisplay(displayedMusic);
+    }
+
+    function searchByTitle(title){
+        let displayedMusic = props.musicLibrary.filter(function(song){
+            return song.title.toLowerCase().includes(title.toLowerCase());
+        })
+        setMusicToDisplay(displayedMusic);
+    }
+
+    function searchByReleaseDate(releaseDate){
+        let displayedMusic = props.musicLibrary.filter(function(song){
+            return song.releaseDate.includes(releaseDate);
+        })
+        setMusicToDisplay(displayedMusic);
+    }
 
     return ( 
         <div>
-            <SearchBar searchByKeyword={searchByKeyword}/>
+            <SearchBar searchByKeyword={searchByKeyword} searchByGenre={searchByGenre} searchByArtist={searchByArtist} searchByAlbum={searchByAlbum} searchByTitle={searchByTitle} searchByReleaseDate={searchByReleaseDate}/>
             <MusicTable musicToDisplay={musicToDisplay}/>
         </div>
     );
---
+++ src/Components/PageHeader/PageHeader.jsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+
+const PageHeader = (props) => {
+
+    const [searchType, setSearchType] = useState('keyword');
+
+    return ( 
+
+     );
+}
+ 
+export default PageHeader;
