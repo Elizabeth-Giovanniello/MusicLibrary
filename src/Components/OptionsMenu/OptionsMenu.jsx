7a1be34c4dbb9375a8f034435263115b011eae8b
Lizzy <ea.giovanniello@gmail.com>
2022-02-17 21:04:47 -0500

    Styled menu dropdown button.

--- src/Components/OptionsMenu/OptionsMenu.jsx
+++ src/Components/OptionsMenu/OptionsMenu.jsx
@@ -1,16 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { DropdownButton, Dropdown } from 'react-bootstrap';
 import { ThreeDotsVertical, ThreeDots } from 'react-bootstrap-icons';
 import EditSongDetails from '../EditSongDetails/EditSongDetails';
 import DeleteSong from '../DeleteSong/DeleteSong';
+import './OptionsMenu.css'
 
 
 const OptionsMenu = (props) => {
 
+    const [isHighlighted, setIsHighlighted] = useState(false);
+
+
     return ( 
-        <Dropdown>
-            <Dropdown.Toggle variant="basic" className='text-danger float-start me-3 p-0'>
-                <ThreeDotsVertical className='text-danger float-start me-3 p-0'></ThreeDotsVertical>
+        <Dropdown drop='start'>
+            <Dropdown.Toggle variant="basic" className='float-start me-3 p-0'>
+                <ThreeDotsVertical className={'me-3 p-0' + (isHighlighted ? " text-danger" : " text-secondary")} onMouseEnter={()=>setIsHighlighted(true)} onMouseLeave={()=>setIsHighlighted(false)}></ThreeDotsVertical>
             </Dropdown.Toggle>
             <Dropdown.Menu className="p-0">
                 <Dropdown.Item className="p-0">
